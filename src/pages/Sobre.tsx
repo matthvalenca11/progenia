3ab90d1da9d3280c014ad9983a4c542d84cf0bbf
matthvalenca11 <98474454+matthvalenca11@users.noreply.github.com>
2026-03-25 15:35:09 -0300

    about update

--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -160,6 +160,10 @@ const Sobre = () => {
       {(() => {
         const firstCtaId = sections.find((s) => s.section_type === "cta")?.id;
         return sections.map((section) => {
+          if (section.section_type === "hero") {
+            return null;
+          }
+
           const showPartnersAndTeamBeforeCta =
             section.section_type === "cta" && section.id === firstCtaId;
 
